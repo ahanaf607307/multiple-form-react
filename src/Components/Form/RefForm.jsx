24b0d2bfa13b379b0521fc5464a935b8e0f9518d
Ahanaf Mubasshir <123632951+ahanaf607307@users.noreply.github.com>
2024-10-31 14:57:14 +0600

    ref form compleate

--- src/Components/Form/RefForm.jsx
+++ src/Components/Form/RefForm.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
 
 function RefForm() {
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passRef = useRef(null);
+  useEffect(() => {
+    nameRef.current.focus()
+  },[])
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(nameRef.current.value);
+    console.log(emailRef.current.value);
+    console.log(passRef.current.value);
+  };
+
   return (
-    <div>RefForm</div>
-  )
+    <div>
+      <h1 className="text-5xl mb-3">Ref Form</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          ref={nameRef}
+          name="name"
+          placeholder="Name"
+          class="input input-bordered w-full max-w-xs"
+        />
+        <input
+          ref={emailRef}
+          type="email"
+          name="email"
+          placeholder="Email"
+          class="input input-bordered w-full max-w-xs"
+          defaultValue="@gmail.com"
+        />
+        <input
+          ref={passRef}
+          type="password"
+          name="password"
+          placeholder="Password"
+          class="input input-bordered w-full max-w-xs"
+        />
+
+        <button class="btn btn-accent text-white w-24">
+          <input type="submit" value="Submit" />{" "}
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default RefForm
+export default RefForm;
